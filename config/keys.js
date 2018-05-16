6ec705fd83fd87a674c99be371a01eefc152a3a9
Nuno Morgado <scolpit@gmail.com>
2018-05-16 20:12:14 +0100

    2

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://nuno:1234@ds151452.mlab.com:51452/devconnectordb'
+};
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+const db = require('./config/keys').mongoURI;
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
-});
+mongoose.connect(db)
+  .then(() => {console.log('MongoDB connected')})
+  .catch((err) => {console.log(err)});
+
+// Routes
+app.get('/', (req, res) => {res.send('It Works!');});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
