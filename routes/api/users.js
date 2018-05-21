59f88e3b7f61a2e0fb42e619a86956fb90c84488
Nuno Morgado <scolpit@gmail.com>
2018-05-21 13:41:06 +0100

    1

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Test users route
 // @access  Public
-router.get('/test',(req, res) => {
-  res.json({ msg: 'users router works!' })
+router.get("/test", (req, res) => {
+  res.json({ msg: "users router works!" });
+});
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email });
 });
 
 module.exports = router;
