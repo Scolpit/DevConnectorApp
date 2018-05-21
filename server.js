5d8d47fb4c2d22b20d76ebacab2296e99069edbb
Nuno Morgado <scolpit@gmail.com>
2018-05-21 19:48:56 +0100

    1

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
-mongoose.connect(db)
-  .then(() => {console.log('MongoDB connected')})
-  .catch((err) => {console.log(err)});
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // Routes
-app.get('/', (req, res) => {res.send('It Works!');});
+app.get("/", (req, res) => {
+  res.send("It Works!");
+});
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
+// Listeners
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
