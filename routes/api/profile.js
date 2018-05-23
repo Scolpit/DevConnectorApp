744efc54c96648895b410661412c7cd501e1dfc2
Nuno Morgado <scolpit@gmail.com>
2018-05-23 09:01:32 +0100

    1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 // @route   GET api/profile/test
 // @desc    Test profile route
 // @access  Public
-router.get('/test',(req, res) => {
-  res.json({ msg: 'profile router works!' })
+router.get("/test", (req, res) => {
+  res.json({ msg: "profile router works!" });
 });
+
+// @route   GET api/profile/test
+// @desc    Test profile route
+// @access  Public
 
 module.exports = router;
