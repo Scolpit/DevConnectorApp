c199f5f8db38d38fb3c0a1639aaae4efeb40fc6a
Nuno Morgado <scolpit@gmail.com>
2018-05-24 21:28:23 +0100

    1

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/Post");
+
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Test posts route
 // @access  Public
-router.get('/test',(req, res) => {
-  res.json({ msg: 'posts router works!' })
+router.get("/test", (req, res) => {
+  res.json({ msg: "posts router works!" });
 });
+
+// @route   GET api/posts/
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(profiles => res.json(profiles))
+    .catch(err => res.status(404).json({ nopostsfound: "Posts not found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(profile => res.json(profile))
+    .catch(err => res.status(404).json({ nopostfound: "Post not found" }));
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by id
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findOne({ _id: req.params.id, user: req.user.id })
+      .then(post => {
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(401).json({ nopostfound: "Not authorized" }));
+  }
+);
+
+// @route   Post api/posts/
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => {
+      res.json(post);
+    });
+  }
+);
 
 module.exports = router;
