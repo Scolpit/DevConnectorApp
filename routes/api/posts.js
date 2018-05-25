855d171ae76df37b53a0c1d6aaf58fe18624fb8c
Nuno Morgado <scolpit@gmail.com>
2018-05-25 11:35:37 +0100

    1

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -128,4 +128,35 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment post by post id
+// @access  Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          user: req.user.id,
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar
+        };
+
+        post.comments.unshift(newComment);
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err => res.status(404).json(err));
+      })
+      .catch(err => {
+        res.status(404).json({ postnotfound: "Post not found" });
+      });
+  }
+);
+
 module.exports = router;
