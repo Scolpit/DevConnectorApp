da7ee03989efed239585296a7fbca52cc581cdef
Nuno Morgado <scolpit@gmail.com>
2018-05-28 11:22:47 +0100

    1

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,32 @@
-import TEST_DISPATCH from "./types";
+import { GET_ERRORS } from "./types";
+import axios from "axios";
 
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get user token
+export const LoginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
