9422678f16d118074633e8ed8747d9c8fc94ae7a
Nuno Morgado <scolpit@gmail.com>
2018-05-28 13:21:43 +0100

    1

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -15,13 +18,15 @@ export const registerUser = (userData, history) => dispatch => {
 };
 
 // Login - Get user token
-export const LoginUser = userData => dispatch => {
+export const loginUser = userData => dispatch => {
   axios
     .post("/api/users/login", userData)
     .then(res => {
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err =>
       dispatch({
@@ -29,4 +34,19 @@ export const LoginUser = userData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Set current User
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// Log out user
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
 };
