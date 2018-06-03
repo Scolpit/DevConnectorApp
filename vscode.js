3f69604898e9ca8b5732d318055bf79fbdd5dde6
Nuno Morgado <scolpit@gmail.com>
2018-06-03 18:42:54 +0100

    1

---
+++ vscode.js
@@ -0,0 +1,21 @@
+// {
+//   "editor.tabSize": 2,
+//   "emmet.includeLanguages": {
+//       "javascript": "javascriptreact"
+//   },
+//   "emmet.syntaxProfiles": {
+//       "javascript": "jsx"
+//   },
+//   "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe",
+//   "git.confirmSync": false,
+//   "git.autofetch": true,
+//   "git.enableSmartCommit": true,
+//   "editor.formatOnSave": true
+// }
+
+// VSCODE Extensions:
+// - Bracket Pair Colorizer
+// - ES7 React/Redux/GraphQL/React-Native Snippets
+// - Live Server
+// - Node.js Modules Intellisense
+// - Prettier - Code formatter
