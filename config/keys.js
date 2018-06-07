1826983a64a12f795764e4c1d109a57ca670f03a
Nuno Morgado <scolpit@gmail.com>
2018-06-07 20:45:33 +0100

    Ready for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://nuno:1234@ds151452.mlab.com:51452/devconnectordb",
-  secretOrKey: "7e73bab0-fb0d-4e9f-857b-a121f51531ba"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://nuno:1234@ds151452.mlab.com:51452/devconnectordb",
+  secretOrKey: "7e73bab0-fb0d-4e9f-857b-a121f51531ba"
+};
